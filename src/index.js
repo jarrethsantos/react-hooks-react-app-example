a6d7bf6911a620ea5ee561bbdf0945e5b5cc4fd3
Jarreth Santos <santos.jarreth@gmail.com>
2025-02-17 13:27:13 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Test from "./components/TestComponent";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
